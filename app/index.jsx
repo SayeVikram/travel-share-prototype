b9ce0c57d239634daede1328c8d3a913dd186514
Saye Vikram <87774705+SayeVikram@users.noreply.github.com>
2025-06-22 23:05:25 -0500

    refactor stylesheet

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React, {useState} from 'react';

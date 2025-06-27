8c80902fc76e648112199b993b0734931340d28f
Saye Vikram <87774705+SayeVikram@users.noreply.github.com>
2025-06-27 14:43:08 -0500

    tab navigation skeleton

--- Screens/SampleScreen.jsx
+++ Screens/SampleScreen.jsx
@@ -1,14 +1,105 @@
-import React from 'react';
-import { Text, View, Image } from "react-native";
-import { TextInput } from 'react-native';
-import { Button } from 'react-native';
-import { supabase } from '../lib/supabase';
-import { Alert } from 'react-native';
-import { useNavigation, CommonActions } from '@react-navigation/native';
+import { Platform, Alert, Text, View, Image, TextInput, StyleSheet, Button, ScrollView, StatusBar, KeyboardAvoidingView } from "react-native";
+import { useLinkBuilder, useTheme } from '@react-navigation/native';
+import {PlatformPressable } from '@react-navigation/elements';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { supabase } from "../lib/supabase";
+import { CommonActions } from "@react-navigation/native";
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+
+const Tab = createBottomTabNavigator();
+
+function MyTabBar({ state, descriptors, navigation }) {
+  const { colors } = useTheme();
+  const { buildHref } = useLinkBuilder();
+
+  return (
+    <View style={{ flexDirection: 'row'}}>
+      {state.routes.map((route, index) => {
+        
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+              ? options.title
+              : route.name;
+
+        const isFocused = state.index === index;
+
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <PlatformPressable
+            href={buildHref(route.name, route.params)}
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarButtonTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1, alignItems:"center", justifyContent:"center" }}
+          >
+            <Text style={{ color: isFocused ? colors.primary : "white" }}>
+              {label}
+            </Text>
+          </PlatformPressable>
+        );
+      })}
+    </View>
+  );
+}
+
+function SampleScreen() {
+  return (
+    <SafeAreaProvider>
+        <SafeAreaView style={{flex:1, backgroundColor:"black", paddingTop: StatusBar.currentHeight}}>
+            <Tab.Navigator
+            tabBar={(props) => <MyTabBar {...props} />}
+            >
+            <Tab.Screen name="Landing" component={LandingScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
+            </Tab.Navigator>
+        </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+export default SampleScreen;
+
+
+const LandingScreen = ({navigation}) => {
+
+    
+
+    return(
+        <View>
+            <Text>IGFEWYGYEW</Text>
+        </View>
+    )
+}
 
 
 
-const SampleScreen = ({navigation}) => {
+
+const ProfileScreen = ({navigation}) => {
 
     const signOut = async () => {
         const { error } = await supabase.auth.signOut()
@@ -29,9 +120,9 @@ const SampleScreen = ({navigation}) => {
 
     return(
         <View>
+
             <Button title='CLICK TO SGIRN OUT' onPress={() => signOut()}/>
         </View>
     )
 }
 
-export default SampleScreen
